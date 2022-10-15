2201092fb9d536581c93d81b16dc29a1ce694d72
Hans Peter Kronholm <112431206+hpkk2000@users.noreply.github.com>
2022-10-15 17:34:32 +0200

    Update max.c

--- src/max.c
+++ src/max.c
@@ -3,8 +3,11 @@
 
 int max(int* numbers, int size)
 {
-    // Excercise 2
-    // Implement your code below...
-
-    return -1;
+    int max = numbers[0]; // array of given size
+    for (int i = 0; i < size; i++) { //Counter through the elements of array
+        if (numbers[i] > max) {//If the current element of array is larger than max value
+            max = numbers[i]; // sets the max value to the element of array
+        }
+    }
+    return max; //returning the max value
 }
